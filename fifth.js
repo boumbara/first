d50bb3e5b47169435566f2830039265331e1f551
boumbara <90278689+boumbara@users.noreply.github.com>
2021-09-09 23:43:38 +0100

    Create fifth.js

---
+++ fifth.js
@@ -0,0 +1,8 @@
+   define(["alpha"], function (alpha) {
+       return {
+         doSomething: function(){
+          console.log("Something was done. ");
+          return 10;
+        }
+      };
+   });
