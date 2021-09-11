32299c5303f511b55bd23e0375c48011c282893f
boumbara <90278689+boumbara@users.noreply.github.com>
2021-09-11 18:14:02 +0100

    Create f0003.js

---
+++ f0003.js
@@ -0,0 +1,11 @@
+define('myModule', 
+    function () {
+        // create your module here
+        var myModule = {
+            doStuff:function(){
+                console.log('Yay! Stuff');
+            }
+        }
+ 
+        return myModule;
+});
