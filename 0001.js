c1ab44ba5b2416587323844b7f92dcb804022b92
boumbara <90278689+boumbara@users.noreply.github.com>
2021-09-11 17:54:37 +0100

    Update 0001.js

--- 0001.js
+++ 0001.js
@@ -1,1 +1,9 @@
-
+define([], function () {
+    function _doSomething() {
+       console.log("doSomething called.");
+    }
+    // Return an object that exposes two functions
+    return {
+        doSomething: _doSomething
+    }
+});
